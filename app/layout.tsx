c533e65e3cbfb7a6405425f3ba6c30a5325949c5
Min Kaung sett <moneumpunk44@gmail.com>
2023-11-06 09:17:38 +0630

    update

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+
+import { relative } from 'path'
+import './globals.css'
+import { Footer, Navbar } from '@/components'
+
+
+
+export const metadata = {
+  title: 'Carhub',
+  description: 'Discover the best car in the world.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className='relative'>
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
+    </html>
+  )
+}
